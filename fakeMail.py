0e122bd623e8388bb7d15484681b77fea88f3ba5
rl1987 <rl1987@users.noreply.github.com>
2022-01-29 19:16:53 +0200

    Run black to fix formatting

--- fakeMail.py
+++ fakeMail.py
@@ -3,8 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def getFakeMail():
-    url = 'https://email-fake.com/'
+    url = "https://email-fake.com/"
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
     mail = soup.find_all("span", {"id": "email_ch_text"})
--- getVerifCode.py
+++ getVerifCode.py
@@ -2,20 +2,21 @@
 
 import time
 
+
 def getInstVeriCode(mailName, domain, driver):
 
-    INST_CODE = 'https://email-fake.com/' + domain + '/' + mailName
-    
+    INST_CODE = "https://email-fake.com/" + domain + "/" + mailName
+
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
     driver.get(INST_CODE)
-    
+
     # button = browser.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a/button").click()
     # time.sleep(3)
     t = driver.title
 
     while True:
-        if t[:4]=="Fake":
+        if t[:4] == "Fake":
             driver.refresh()
             t = driver.title
             print(t)
@@ -28,25 +29,29 @@ def getInstVeriCode(mailName, domain, driver):
     code = t[:6]
     driver.switch_to.window(driver.window_handles[0])
     return code
-    
+
 
 def getInstVeriCodeDouble(mailName, domain, driver, oldCode):
 
-    INST_CODE = 'https://email-fake.com/' + domain + '/' + mailName
-    
+    INST_CODE = "https://email-fake.com/" + domain + "/" + mailName
+
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
     driver.get(INST_CODE)
     time.sleep(4)
 
     print(t)
-    code = driver.find_element_by_xpath("/html/body/div[3]/div/div/div[1]/div[2]/a[1]/div[2]").text
+    code = driver.find_element_by_xpath(
+        "/html/body/div[3]/div/div/div[1]/div[2]/a[1]/div[2]"
+    ).text
     while oldCode == code:
         driver.refresh()
-        print('Whait for new code!')
+        print("Whait for new code!")
         time.sleep(1)
-        code = driver.find_element_by_xpath("//*[@id='email-table']/div[2]/div[1]/div/h1").text
-    
+        code = driver.find_element_by_xpath(
+            "//*[@id='email-table']/div[2]/div[1]/div/h1"
+        ).text
+
     codeNew = code[:6]
     driver.switch_to.window(driver.window_handles[0])
     return codeNew
